ef8a339499bf9b4c045c1707174213eecb1ad2e6
= <56686212+Jashwanth63@users.noreply.github.com>
2024-05-25 11:57:07 -0400

    25/05SplitStringLC

--- sample.cpp
+++ sample.cpp
@@ -1,11 +1,75 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
+#include<climits>
+#include<vector>
+#include<unordered_set>
 
 
 using namespace std;
 
+    
+void findWord(vector<vector<string>> &ans, string &x, vector<string> temp, unordered_set<string> &words, int i, int j, int &len, bool isPresent){
+    if(i  >= len){
+        //cout<<"x";
+        if(isPresent){
+            //cout<<"x";
+            
+            //cout<<"x";
+            ans.push_back(temp);
+        }
+        return;
+    }
+    isPresent = false;
+    for(int z = 0; (z+i)<len; z++){
+        //cout<<"working";
+        string curr = x.substr(i, z+1);
+        cout<<i<<" "<<z<<" ";
+        cout<<curr<<" "<<endl;
+        if(words.find(curr) != words.end()){ //If found
+            isPresent = true;
+            //cout<<endl<<curr<<endl;
+            temp.push_back(curr);
+            findWord(ans, x, temp, words, i+z+1, 0, len, isPresent);
+            temp.pop_back();
+            isPresent = false;
+            //findWord(ans, x, temp, words, i, z+1, len, false);
+            //cout<<"X";
+        }   
+    }
+    
+    return;
+}
+
+
+vector<string> displayVec(vector<vector<string>> &ans){
+    vector<string> final;
+    for(auto sentence: ans){
+        string curr;
+        for(auto word: sentence){
+            curr += word + " ";
+        }
+        cout<<curr;
+        final.push_back(curr);
+        cout<<endl;
+    }
+    return final;
+}
+
+
 int main(){
-    string x = "abc";
-    for(char &t : x) cout<<t<<" ";
+    unordered_set<string> words{"apple", "pen", "epen", "appl"};
+    string x = "penapplepen";
+    vector<vector<string>> ans;
+    int len = x.length();
+    bool isPresent = false;
+    vector<string> temp;
+    
+    findWord(ans, x, temp, words, 0, 0, len, isPresent);
+
+    //display1D(converttoSentence);
+    //cout<<"done";
+    vector<string> final = displayVec(ans);
+
     return 0;
+
 }
---
+++ strings/sample2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string x = "abcde";
+	cout<<x.substr(0, 4);
+	return 0;
+}
